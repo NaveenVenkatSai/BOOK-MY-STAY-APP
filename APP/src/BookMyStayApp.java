b2ce4695c575b63fa22f87431e84199dc968e45c
NaveenVenkatSai <naveenvenkatsai28@gmail.com>
2026-03-20 16:06:12 +0530

    Reservation Confirmation & Room Allocation

--- APP/src/BookMyStayApp.java
+++ APP/src/BookMyStayApp.java
@@ -1,38 +1,35 @@
 import java.util.HashMap;
 import java.util.Scanner;
-
 public class BookMyStayApp{
-
     public static void main(String[] args) {
-
-        // Room inventory
         HashMap<String, Integer> roomInventory = new HashMap<>();
         roomInventory.put("Single Room", 5);
         roomInventory.put("Double Room", 3);
         roomInventory.put("Deluxe Room", 2);
-
+        HashMap<String, Integer> roomNumbers = new HashMap<>();
+        roomNumbers.put("Single Room", 101);
+        roomNumbers.put("Double Room", 201);
+        roomNumbers.put("Deluxe Room", 301);
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Welcome to the Hotel Booking Management System");
-
         System.out.print("Enter room type to book: ");
         String roomType = scanner.nextLine();
-
         if (roomInventory.containsKey(roomType)) {
             int available = roomInventory.get(roomType);
-
             if (available > 0) {
-                // Booking logic (FCFS)
+                int allocatedRoom = roomNumbers.get(roomType);
                 roomInventory.put(roomType, available - 1);
-                System.out.println("Booking confirmed for " + roomType);
-                System.out.println("Remaining rooms: " + roomInventory.get(roomType));
+                roomNumbers.put(roomType, allocatedRoom + 1);
+                System.out.println("\nBooking Confirmed!");
+                System.out.println("Room Type: " + roomType);
+                System.out.println("Allocated Room Number: " + allocatedRoom);
+                System.out.println("Remaining Rooms: " + roomInventory.get(roomType));
             } else {
                 System.out.println("Sorry, " + roomType + " is fully booked.");
             }
         } else {
             System.out.println("Invalid room type.");
         }
-
         scanner.close();
     }
 }
