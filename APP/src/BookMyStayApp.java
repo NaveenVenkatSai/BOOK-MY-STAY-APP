366786fadc341e3333f06f66a619f9c917c9b41c
NaveenVenkatSai <naveenvenkatsai28@gmail.com>
2026-03-20 15:29:18 +0530

    Booking Request

--- APP/src/BookMyStayApp.java
+++ APP/src/BookMyStayApp.java
@@ -1,25 +1,38 @@
 import java.util.HashMap;
 import java.util.Scanner;
-public class BookMyStayApp {
+
+public class BookMyStayApp{
+
     public static void main(String[] args) {
+
+        // Room inventory
         HashMap<String, Integer> roomInventory = new HashMap<>();
         roomInventory.put("Single Room", 5);
         roomInventory.put("Double Room", 3);
         roomInventory.put("Deluxe Room", 2);
+
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Welcome to the Hotel Booking Management System");
-        System.out.print("Enter room type to search: ");
-        String searchRoom = scanner.nextLine();
-        if (roomInventory.containsKey(searchRoom)) {
-            int available = roomInventory.get(searchRoom);
+
+        System.out.print("Enter room type to book: ");
+        String roomType = scanner.nextLine();
+
+        if (roomInventory.containsKey(roomType)) {
+            int available = roomInventory.get(roomType);
+
             if (available > 0) {
-                System.out.println(searchRoom + " is available. Rooms left: " + available);
+                // Booking logic (FCFS)
+                roomInventory.put(roomType, available - 1);
+                System.out.println("Booking confirmed for " + roomType);
+                System.out.println("Remaining rooms: " + roomInventory.get(roomType));
             } else {
-                System.out.println(searchRoom + " is currently not available.");
+                System.out.println("Sorry, " + roomType + " is fully booked.");
             }
         } else {
             System.out.println("Invalid room type.");
         }
+
         scanner.close();
     }
 }
